a55290008479fb9ff78fafa50a086ea05e9d4e29
Guilherme <guilherme.studier18@gmail.com>
2023-08-22 01:19:05 -0300

    ceos - add to SystemAccessCancellation function

--- src/features/systemAccessCancellation/components/SystemAccessCancellationForm.vue
+++ src/features/systemAccessCancellation/components/SystemAccessCancellationForm.vue
@@ -90,9 +90,13 @@ export default {
 
       try {
         await this.createUser(userData)
-        this.toast.success('Cadastro efetuado criado com sucesso')
+        this.toast.success(
+          'Cancelamento de acesso ao sistema efetuado com sucesso'
+        )
       } catch (error) {
-        this.toast.warning('Não foi possível realizar o cadastro')
+        this.toast.warning(
+          'Não foi possível realizar cancelamento de acesso ao sistema'
+        )
       }
       this.clearForm()
     },
