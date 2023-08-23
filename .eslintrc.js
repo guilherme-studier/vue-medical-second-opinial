06d1a65ef14650b363c530a952bc848ec5f4e79d
Thiago dos Santos Studier <thiago.studier@pmweb.local>
2023-08-23 16:27:37 -0300

    staging: fix errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,12 +10,12 @@ module.exports = {
   rules: {
     'no-console': 'warn',
     'no-debugger': 'warn',
-    semi: ['warn', 'never'],
-    quotes: ['warn', 'single'],
-    'no-var': 'warn',
-    'prefer-const': 'warn',
-    eqeqeq: ['warn', 'always'],
-    camelcase: 'warn',
+    semi: ['error', 'never'],
+    quotes: ['error', 'single'],
+    'no-var': 'error',
+    'prefer-const': 'error',
+    eqeqeq: ['error', 'always'],
+    camelcase: 'error',
     'vue/multi-word-component-names': 0
   },
   overrides: [
