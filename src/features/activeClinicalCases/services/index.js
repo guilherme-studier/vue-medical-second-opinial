83e0dd318e85385129494404bdd157ee41dee86c
Guilherme <guilherme.studier18@gmail.com>
2023-11-12 23:34:16 -0300

    ceos - add to activeVoucher loading

--- src/features/activeClinicalCases/services/index.js
+++ src/features/activeClinicalCases/services/index.js
@@ -6,10 +6,29 @@ const USER_BASE_URL = 'https://meso.poatech.com.br:450/user/api/1.0'
 const getToken = () => store?.getters?.getUserToken
 
 /**
+ * Salvar desenvolvimento do Voucher
+ * Endpoint para editar um caso clínico/voucher
+ */
+export const saveVoucher = (questionnaire, voucherId) => {
+  return axios({
+    method: 'PUT',
+    headers: {
+      'content-type': 'application/json',
+      Authorization: `Bearer ${getToken()}`
+    },
+    url: `${USER_BASE_URL}/voucher/${voucherId}`,
+    data: {
+      questionnaire: questionnaire,
+      completed: false
+    }
+  })
+}
+
+/**
  * Ativar Voucher
  * Endpoint para editar um caso clínico/voucher
  */
-export const putVoucher = (questionnaire, voucherId) => {
+export const activeVoucher = (questionnaire, voucherId) => {
   return axios({
     method: 'PUT',
     headers: {
